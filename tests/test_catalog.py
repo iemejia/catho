0005d2399ef701c57281c9472ebf639918437833
Ismael Mejia <iemejia@gmail.com>
2013-02-27 21:16:01 +0100

    Added initial testcases

---
+++ tests/test_catalog.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import unittest
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from catho import catho
+
+class TestCatalog(unittest.TestCase):
+    def setUp(self):
+        return
+    def test_catalog_creation(self):
+        # self.assertEqual(md5(sub[2]), '4cd9278a35ba2305f47354ee13472260')
+        return
+
+if __name__ == '__main__':
+    unittest.main()
