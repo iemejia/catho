dd37d913c0ed96f0a50289a1e1573be962917a53
Ismael Mejia <iemejia@gmail.com>
2013-02-28 09:10:17 +0100

    Refactor method names prefixed by db or file depending on operations. Increased privacy in methods.

--- catho/catho.py
+++ catho/catho.py
@@ -23,94 +23,20 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
-def touch_catho_dir():
+sql_insert_metadata = 'INSERT INTO METADATA (key, value) VALUES (?,?)'
+sql_insert_catalog = 'INSERT INTO CATALOG (name, date, size, path, hash) VALUES (?,?,?,?,?)'
+sql_select_metadata = "SELECT * FROM METADATA;"
+sql_select_catalog = "SELECT * FROM CATALOG;"
+
+# file functions
+def file_touch_catho_dir():
     if not os.path.exists(catho_path):
         os.makedirs(catho_path)
 
-def get_catalog_abspath(name):
+def file_get_catalog_abspath(name):
     return catho_path + name + catho_extension
 
-def __create_metadata(name, metadata):
-    """Insert metadata from the metadata dictionnary"""
-    try:
-        conn = sqlite3.connect(get_catalog_abspath(name))
-        c = conn.cursor()
-        c.execute("DROP TABLE IF EXISTS METADATA;")
-        c.execute("CREATE TABLE METADATA (key TEXT, value TEXT);")
-        c.executemany('INSERT INTO METADATA (key, value) VALUES (?,?)', metadata)
-        conn.commit()
-        conn.close()
-    except sqlite3.Error as e:
-        logger.error("An error occurred:", e)
-
-def create_metadata(name, path):
-    date = str(int(time.time()))
-    metadata = [('version', '1'), ('name', name), ('path', path), ('createdate', date), ('lastmodifdate', date)]
-    return __create_metadata(name, metadata)
-
-def create_catalog(name, files):
-    try:
-        conn = sqlite3.connect(get_catalog_abspath(name))
-        conn.text_factory = str
-        c = conn.cursor()
-        c.execute("DROP TABLE IF EXISTS CATALOG;")
-        c.execute("CREATE TABLE CATALOG (id INT PRIMARY KEY ASC, name TEXT NOT NULL, date INT NOT NULL, size INT NOT NULL, path TEXT NOT NULL, hash TEXT);")
-        c.executemany('INSERT INTO CATALOG (name, date, size, path, hash) VALUES (?,?,?,?,?)', files)
-        conn.commit()
-        conn.close()
-    except sqlite3.Error as e:
-        logger.error("An error occurred:", e.args[0])
-
-def get_catalogs():
-    catalogs = []
-    files = os.listdir(catho_path)
-    for filename in files:
-        if filename.endswith(catho_extension):
-            size, date = get_file_info(catho_path, filename)
-            catalogs.append((filename[:-3], size, date))
-    return catalogs
-
-def __get_all(name, query):
-    """Generic query invocation in name db"""
-    rows = []
-    try:
-        conn = sqlite3.connect(get_catalog_abspath(name))
-        conn.text_factory = str
-        c = conn.cursor()
-        c.execute(query)
-        rows = c.fetchall()
-        conn.close()
-    except sqlite3.Error as e:
-        logger.error("An error occurred:", e)
-    return rows
-
-def get_metadata(name):
-    """Returns a tuple (key, value) with the metadata"""
-    query = "SELECT * FROM METADATA;"
-    return __get_all(name, query)
-
-def get_catalog(name):
-    """Returns a tuple of the catalog, a row corresponds to a file"""
-    query = "SELECT * FROM CATALOG;"
-    return __get_all(name, query)
-
-def metadata_str(name):
-    meta = get_metadata(name)
-    s = "METADATA\n"
-    s += '\n'.join('%s: \t%s' % (key, value) for (key, value) in meta)
-    return s + '\n'
-
-def catalog_str(name):
-    catalog = get_catalog(name)
-    s = "CATALOG\n"
-    s += '\n'.join('%s\t%s\t%s\t%s\t%s' % (name, str(datetime.fromtimestamp(date)), size, path, hash) for (id, name, date, size, path, hash) in catalog)
-    return s + '\n'
-
-def create_db(name, path, files):
-    create_metadata(name, path)
-    create_catalog(name, files)
-
-def get_sha1(filename):
+def file_get_sha1(filename):
     sha1 = hashlib.sha1()
     f = open(filename, 'rb')
     try:
@@ -119,7 +45,16 @@ def get_sha1(filename):
         f.close()
     return sha1.hexdigest()
 
-def get_filelist(orig_path, compute_hash = False):
+def file_get_catalogs():
+    catalogs = []
+    files = os.listdir(catho_path)
+    for filename in files:
+        if filename.endswith(catho_extension):
+            size, date = get_file_info(catho_path, filename)
+            catalogs.append((filename[:-3], size, date))
+    return catalogs
+
+def file_get_filelist(orig_path, compute_hash = False):
     # todo verify if stat gives the same value in windows
     files = []    
     for dirname, dirnames, filenames in os.walk(orig_path):
@@ -133,7 +68,7 @@ def get_filelist(orig_path, compute_hash = False):
                 if compute_hash and not os.path.isdir(filename):
                     # todo should we use the git convention for this ?
                     # sha1("blob " + filesize + "\0" + data)
-                    hash = get_sha1(fullpath)
+                    hash = file_get_sha1(fullpath)
                     # logger.debug("SHA1 = %s" % hash)
                 files.append((filename, date, size, path, hash))
             except OSError as oe:
@@ -142,12 +77,99 @@ def get_filelist(orig_path, compute_hash = False):
                 logger.error("An error occurred:", ue)
     return files
 
+def file_rm_catalog_file(catalogs):
+    """ deletes the list of cats """
+    filelist = [ glob.glob(file_get_catalog_abspath(f)) for f in catalogs ]
+    filelist = sum(filelist, [])
+    for f in filelist:
+        try:
+            os.remove(f)
+            logger.info("rm %s" % f)
+        except OSError:
+            logger.error("rm: %s: No such file or directory" % f)
+
+# db functions
+def __db_create_schema(name):
+    """Creates the db schema"""
+    try:
+        conn = sqlite3.connect(file_get_catalog_abspath(name))
+        c = conn.cursor()
+        c.execute("DROP TABLE IF EXISTS METADATA;")
+        c.execute("CREATE TABLE METADATA (key TEXT, value TEXT);")
+        c.execute("DROP TABLE IF EXISTS CATALOG;")
+        c.execute("CREATE TABLE CATALOG (id INT PRIMARY KEY ASC, name TEXT NOT NULL, date INT NOT NULL, size INT NOT NULL, path TEXT NOT NULL, hash TEXT);")
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        logger.error("An error occurred:", e)
+
+def __db_insert(name, query, l):
+    """Generic insert invocation in name db"""
+    try:
+        conn = sqlite3.connect(file_get_catalog_abspath(name))
+        c = conn.cursor()
+        c.executemany(query, l)
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        logger.error("An error occurred:", e)
+
+
+def __db_get_all(name, query):
+    """Generic query invocation in name db"""
+    rows = []
+    try:
+        conn = sqlite3.connect(file_get_catalog_abspath(name))
+        conn.text_factory = str
+        c = conn.cursor()
+        c.execute(query)
+        rows = c.fetchall()
+        conn.close()
+    except sqlite3.Error as e:
+        logger.error("An error occurred:", e)
+    return rows
+
+def __db_insert_metadata(name, path):
+    """Insert metadata from the metadata dictionary into the database"""
+    date = str(int(time.time()))
+    metadata = [('version', '1'), ('name', name), ('path', path), ('createdate', date), ('lastmodifdate', date)]
+    return __db_insert(name, sql_insert_metadata, metadata)
+
+def __db_insert_catalog(name, files):
+    return __db_insert(name, sql_insert_catalog, files)
+
+def db_create(name, path, files):
+    __db_create_schema(name)
+    __db_insert_metadata(name, path)
+    __db_insert_catalog(name, files)
+
+def db_get_metadata(name):
+    """Returns a tuple (key, value) with the metadata"""
+    return __db_get_all(name, sql_select_metadata)
+
+def db_get_catalog(name):
+    """Returns a tuple of the catalog, a row corresponds to a file"""
+    return __db_get_all(name, sql_select_catalog)
+
+# to string functions
+def metadata_str(name):
+    meta = db_get_metadata(name)
+    s = "METADATA\n"
+    s += '\n'.join('%s: \t%s' % (key, value) for (key, value) in meta)
+    return s + '\n'
+
+def catalog_str(name):
+    catalog = db_get_catalog(name)
+    s = "CATALOG\n"
+    s += '\n'.join('%s\t%s\t%s\t%s\t%s' % (name, str(datetime.fromtimestamp(date)), size, path, hash) for (id, name, date, size, path, hash) in catalog)
+    return s + '\n'
+
 def catalogs_str():
-    catalogs = get_catalogs()
+    catalogs = file_get_catalogs()
     s = ''
     for catalog, size, timestamp in catalogs:
         date = str(datetime.fromtimestamp(timestamp))
-        s += '{: >0} {: >15} {: >15}'.format(*(catalog, size, date))
+        s += '\n{: >0} {: >15} {: >15}'.format(*(catalog, size, date))
     return s
 
 def catalogs_info_str(names):
@@ -157,34 +179,23 @@ def catalogs_info_str(names):
         s += catalog_str(name)
     return s
 
-def del_catalog_file(catalogs):
-    """ deletes the list of cats """
-    filelist = [ glob.glob(get_catalog_abspath(f)) for f in catalogs ]
-    filelist = sum(filelist, [])
-    for f in filelist:
-        try:
-            os.remove(f)
-            logger.info("rm %s" % f)
-        except OSError:
-            logger.error("rm: %s: No such file or directory" % f)
-
 if __name__ == '__main__':
     cmd = sys.argv[1]
 
     if (cmd == 'init'):
-        touch_catho_dir()
+        file_touch_catho_dir()
 
     elif (cmd == 'add'):
         name = sys.argv[2]
         orig_path = sys.argv[3]
         extra_args = sys.argv[4:]
         # we check that the file exists or if it's forced and we create the cat
-        if '-f' in extra_args or not os.path.exists(get_catalog_abspath(name)):
+        if '-f' in extra_args or not os.path.exists(file_get_catalog_abspath(name)):
             logger.info("Creating catalog: %s" % name)
             compute_hash = '-H' in extra_args
             if compute_hash:
                 logger.info("Computing hashes (this will take more time)...")
-            create_db(name, os.path.abspath(orig_path), get_filelist(orig_path, compute_hash))
+            db_create(name, os.path.abspath(orig_path), file_get_filelist(orig_path, compute_hash))
         else:
             logger.error("Catalog: %s already exists" % name)
 
@@ -197,10 +208,12 @@ if __name__ == '__main__':
 
     elif (cmd == 'rm'):
         cats = sys.argv[2:]
-        del_catalog_file(cats)
+        file_rm_catalog_file(cats)
 
     elif (cmd == 'find'):
-        logger.error("TODO find")
+        exprs = sys.argv[2:]
+        if exprs:
+            logger.error("TODO find")
 
     elif (cmd == 'scan'):
         logger.error("TODO scan")
--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -11,57 +11,59 @@ from catho import catho
 
 class TestCatalog(unittest.TestCase):
     def setUp(self):
-        catho.touch_catho_dir()
+        catho.file_touch_catho_dir()
         self.orig_path = '.'
         self.name = 'test'
 
-    def test_get_filelist(self):
-        filelist = catho.get_filelist(self.orig_path)
+    def test_file_get_filelist(self):
+        filelist = catho.file_get_filelist(self.orig_path)
         self.assertTrue(filelist)
         self.assertTrue(len(filelist) > 0)
 
-    def test_get_filelist_hash(self):
-        filelist = catho.get_filelist(self.orig_path, compute_hash=True)
+    def test_file_get_filelist_hash(self):
+        filelist = catho.file_get_filelist(self.orig_path, compute_hash=True)
         self.assertTrue(filelist)
         self.assertTrue(len(filelist) > 0)
         
-    def test_create_db(self):
-        catho.create_db(self.name, os.path.abspath(self.orig_path), catho.get_filelist(self.orig_path))
+    def test_db_create(self):
+        catho.db_create(self.name, os.path.abspath(self.orig_path), catho.file_get_filelist(self.orig_path))
         # self.assertEqual(md5(sub[2]), '4cd9278a35ba2305f47354ee13472260')
 
-    def test_get_metadata(self):
-        metadata = catho.get_metadata(self.name)
+    def test_db_get_metadata(self):
+        metadata = catho.db_get_metadata(self.name)
         self.assertTrue(metadata)
         self.assertTrue(len(metadata) == 5)
         # self.assertTrue(len(filelist) > 0)
 
-    def test_get_catalog(self):
-        catho.create_db(self.name, os.path.abspath(self.orig_path), catho.get_filelist(self.orig_path))
-        catalog = catho.get_catalog(self.name)
+    def test_db_get_catalog(self):
+        catho.db_create(self.name, os.path.abspath(self.orig_path), catho.file_get_filelist(self.orig_path))
+        catalog = catho.db_get_catalog(self.name)
         self.assertTrue(catalog)
         self.assertTrue(len(catalog) > 0)
 
-    def test_get_catalogs(self):
-        catho.create_db(self.name, os.path.abspath(self.orig_path), catho.get_filelist(self.orig_path))
-        catalogs = catho.get_catalogs()
+    def test_file_get_catalogs(self):
+        catho.db_create(self.name, os.path.abspath(self.orig_path), catho.file_get_filelist(self.orig_path))
+        catalogs = catho.file_get_catalogs()
         self.assertTrue(catalogs)
         self.assertTrue(len(catalogs) > 0)
 
-    def test_del_catalog_file(self):
-        catho.create_db(self.name, os.path.abspath(self.orig_path), catho.get_filelist(self.orig_path))
-        catho.del_catalog_file([self.name])
+    def test_file_rm_catalog_file(self):
+        catho.db_create(self.name, os.path.abspath(self.orig_path), catho.file_get_filelist(self.orig_path))
+        catho.file_rm_catalog_file([self.name])
         return
 
     def test_metadata_str(self):
-        # catho.create_db(self.name, os.path.abspath(self.orig_path), catho.get_filelist(self.orig_path))
+        catho.db_create(self.name, os.path.abspath(self.orig_path), catho.file_get_filelist(self.orig_path))
         str = catho.metadata_str(self.name)
         self.assertTrue(str)
 
     def test_catalog_str(self):
+        catho.db_create(self.name, os.path.abspath(self.orig_path), catho.file_get_filelist(self.orig_path))
         str = catho.catalog_str(self.name)
         self.assertTrue(str)
 
     def test_catalogs_str(self):
+        catho.db_create(self.name, os.path.abspath(self.orig_path), catho.file_get_filelist(self.orig_path))
         str = catho.catalogs_str()
         self.assertTrue(str)
 
