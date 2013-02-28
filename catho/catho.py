80119a88af09cb62741389b1c67931b653435fab
Ismael Mejia <iemejia@gmail.com>
2013-02-28 01:34:56 +0100

    Improved create metadata via executemany Refactored create metadata method

--- catho/catho.py
+++ catho/catho.py
@@ -18,14 +18,8 @@ catho_path = home + "/.catho/"
 catho_extension = '.db'
 catalogs = []
 
-# import logging.config
-# logging.config.fileConfig('logging.conf')
-
-# create logger
 logger = logging.getLogger('catho')
 logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
@@ -34,22 +28,24 @@ def touch_catho_dir():
     if not os.path.exists(catho_path):
         os.makedirs(catho_path)
 
-def create_metadata(name, path):
+def __create_metadata(name, metadata):
+    """Insert metadata from the metadata dictionnary"""
     try:
         touch_catho_dir()
         conn = sqlite3.connect(catho_path + name + catho_extension)
         c = conn.cursor()
         c.execute("DROP TABLE IF EXISTS METADATA;")
-        c.execute("CREATE TABLE METADATA(key TEXT, value TEXT);")
-        c.execute("INSERT INTO METADATA (key, value) VALUES('version', '1');")
-        c.execute("INSERT INTO METADATA (key, value) VALUES('name', '" + name + "');")
-        c.execute("INSERT INTO METADATA (key, value) VALUES('path', '" + path + "');")
-        c.execute("INSERT INTO METADATA (key, value) VALUES('createdate', '" + str(int(time.time())) + "');")
-        c.execute("INSERT INTO METADATA (key, value) VALUES('lastmodifdate', '" + str(int(time.time())) + "');")
+        c.execute("CREATE TABLE METADATA (key TEXT, value TEXT);")
+        c.executemany('INSERT INTO METADATA (key, value) VALUES (?,?)', metadata)
         conn.commit()
         conn.close()
     except sqlite3.Error as e:
-        logger.error("An error occurred:", e.args[0])
+        logger.error("An error occurred:", e)
+
+def create_metadata(name, path):
+    date = str(int(time.time()))
+    metadata = [('version', '1'), ('name', name), ('path', path), ('createdate', date), ('lastmodifdate', date)]
+    return __create_metadata(name, metadata)
 
 def create_catalog(name, files):
     try:
@@ -58,7 +54,7 @@ def create_catalog(name, files):
         conn.text_factory = str
         c = conn.cursor()
         c.execute("DROP TABLE IF EXISTS CATALOG;")
-        c.execute("CREATE TABLE CATALOG(id INT PRIMARY KEY ASC, name TEXT NOT NULL, date INT NOT NULL, size INT NOT NULL, path TEXT NOT NULL, hash TEXT);")
+        c.execute("CREATE TABLE CATALOG (id INT PRIMARY KEY ASC, name TEXT NOT NULL, date INT NOT NULL, size INT NOT NULL, path TEXT NOT NULL, hash TEXT);")
         c.executemany('INSERT INTO CATALOG (name, date, size, path) VALUES (?,?,?,?)', files)
         conn.commit()
         conn.close()
@@ -129,19 +125,23 @@ if __name__ == '__main__':
                 date = datetime.fromtimestamp(timestamp)
                 logger.info('{: >0} {: >15} {: >15}'.format(*(catalog, size, str(date))))
         else:
-            logger.debug("TODO: Print catalog info")
-            # pass
+            logger.debug("TODO: List catalog info")
+        del cats
             
     elif (cmd == 'rm'):
-        catalogs = sys.argv[2:]
-        filelist = [ glob.glob(catho_path + f + catho_extension) for f in catalogs ]
+        cats = sys.argv[2:]
+        filelist = [ glob.glob(catho_path + f + catho_extension) for f in cats ]
         filelist = sum(filelist, [])
         for f in filelist:
             try:
                 os.remove(f)
-                logger.info("rm", f)
+                logger.info("rm %s" % f)
             except OSError:
                 logger.error("rm: %s: No such file or directory" % f)
+        del cats
 
     elif (cmd == 'find'):
         logger.error("TODO find")
+
+    elif (cmd == 'scan'):
+        logger.error("TODO scan")
