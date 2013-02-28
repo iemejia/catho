e473e826662150cc8c2455f8135ed5908b0e1fce
Ismael Mejia <iemejia@gmail.com>
2013-02-28 02:10:08 +0100

    Added real test for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,14 +6,17 @@ import unittest
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from catho.utils import *
+from catho import utils
 
 class TestUtils(unittest.TestCase):
+
     def setUp(self):
         return
+
     def test_catalog_creation(self):
-        # self.assertEqual(md5(sub[2]), '4cd9278a35ba2305f47354ee13472260')
+        (size, date) = utils.get_file_info('.', 'test_utils.py')
+        self.assertTrue(size)
+        self.assertTrue(date)
         return
 
 if __name__ == '__main__':
