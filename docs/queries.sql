79114700575d0d1455beeb99998e410e5d481bd2
Ismael Mejia <iemejia@gmail.com>
2013-03-03 22:52:13 +0100

    Fixed error in find duplicates query

--- docs/queries.sql
+++ docs/queries.sql
@@ -1,8 +1,7 @@
 -- some interesting queries
 
 -- find duplicates
-SELECT hash, path % name FROM catalog WHERE hash NOT IN (SELECT hash
-FROM catalog GROUP BY hash HAVING ( COUNT(hash) = 1 )) order by hash;
+SELECT hash, path || name FROM catalog WHERE hash NOT IN (SELECT hash FROM catalog GROUP BY hash HAVING ( COUNT(hash) = 1 )) order by hash;
 
 -- get the 10 biggest files
 SELECT * FROM catalog ORDER BY size DESC LIMIT 10;
