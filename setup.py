0c82e23001fa8e7da2bb0dde43462197f0f19464
Ismael Mejia <iemejia@gmail.com>
2013-04-22 09:55:49 +0200

    Added some missing fields and registered for the first time in pip

--- setup.py
+++ setup.py
@@ -3,7 +3,23 @@ from distutils.core import setup
 setup(
     name='catho',
     version='0.1dev',
+    description = 'Catho file catalog manager',
+    long_description = 'Small command-line program to manage file catalogs.',
+    url = 'https://github.com/iemejia/catho',
+    author = 'Ismael Mejia',
+    maintainer = 'Ismael Mejia',
+    maintainer_email = 'iemejia@gmail.com',
     packages=['catho',],
-    license='LGPL',
-    long_description=open('README.md').read(),
+    license='GNU Lesser General Public License (LGPL)',
+    # long_description=open('README.md').read(),
+    # classifiers = [
+    #     "Topic :: Multimedia :: Video",
+    #     "Development Status :: 5 - Production/Stable",
+    #     "Environment :: Console",
+    #     "License :: Public Domain",
+    #     "Programming Language :: Python :: 2.6",
+    #     "Programming Language :: Python :: 2.7",
+    #     "Programming Language :: Python :: 3",
+    #     "Programming Language :: Python :: 3.3"
+    # ],
 )
