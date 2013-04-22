780cc68ea5436403a2c918ea1ef03c1e9fb73b05
Ismael Mejia <iemejia@gmail.com>
2013-04-22 09:44:52 +0200

    Created first version of the setup file (pip)

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='catho',
+    version='0.1dev',
+    packages=['catho',],
+    license='LGPL',
+    long_description=open('README.md').read(),
+)
